1913492760aff6c5865d8cba908ec60cc096062c
CedricJ2U <cjglumawig.student@ua.edu.ph>
2024-08-27 12:15:10 +0800

    Upated and Rebuff

--- pages/index.js
+++ pages/index.js
@@ -12,19 +12,42 @@ export default function Home() {
           height={150}
           className={styles.profilePic}
         />
-        <h1></h1>
+        <h1>Cedric James Lumawig</h1>
       </header>
       <main className={styles.main}>
         <article className={styles.post}>
           <h2>Introduce Yourself</h2>
-          <p>Cedric James Lumawig,<br/> My Hobbies are Model Kits Building and Sleeping <br/> 
-          Favourite Music In The End by Linkin Park <br/> Favourite Food Ginisang Munggo <br/> I take the IT course that when I got Highschool i got facisnated on Technology and 
-          That my First coding experience was HTML <br/> Probably see myself working as Game Developer, Web Developer or Web Designer but if the competition is high gonna work in Callcenter instead
-          <br/> I already know some basics of HTML and CSS but i want learn more applying Javascript on HTML. <br/> More Advanced area on CSS and Javascript application on the Website.  </p> 
+          <div className={styles.resumeContainer}>
+            <div className={styles.section}>
+              <div className={styles.sectionTitle}>Hobbies</div>
+              <p>Model Kits Building and Sleeping</p>
+            </div>
+            <div className={styles.section}>
+              <div className={styles.sectionTitle}>Favourite Music</div>
+              <p>In The End by Linkin Park</p>
+            </div>
+            <div className={styles.section}>
+              <div className={styles.sectionTitle}>Favourite Food</div>
+              <p>Ginisang Munggo</p>
+            </div>
+            <div className={styles.section}>
+              <div className={styles.sectionTitle}>Education and Experience</div>
+              <p>I take the IT course that when I got Highschool I got fascinated by Technology. My first coding experience was HTML.</p>
+            </div>
+            <div className={styles.section}>
+              <div className={styles.sectionTitle}>Career Goals</div>
+              <p>Probably see myself working as a Game Developer, Web Developer, or Web Designer. However, if the competition is high, I might work in a Call Center instead.</p>
+            </div>
+            <div className={styles.section}>
+              <div className={styles.sectionTitle}>Skills and Learning Goals</div>
+              <p>I already know some basics of HTML and CSS, but I want to learn more about applying JavaScript to HTML and more advanced areas of CSS and JavaScript application on websites.</p>
+            </div>
+          </div>
         </article>
       </main>
       <footer className={styles.footer}>
-        <p>Love Pampanga!</p>
+       <p>(This is a sample website - you'll be building a site like this on</p>
+        <a href='https://nextjs.org/learn'> our Next.js tutorial.) </a>
       </footer>
     </div>
   )
